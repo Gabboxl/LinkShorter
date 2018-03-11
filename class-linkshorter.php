4abf63e055aa27c93b8a42dafcbb075fa7c4e14b
Gabboxl <gabboxl@virgilio.it>
2018-03-11 12:26:56 +0100

    Some fixes

--- class-linkshorter.php
+++ class-linkshorter.php
@@ -1,6 +1,24 @@
 <?php
-
-//linkshorter class made by gabboxl (t.me/gabbo_xl) under license GNU AGPLv3
+/**
+* LinkShorter - A class by Gabboxl made to short links using the most famous link shortener services  under license GNU AGPLv3
+*
+*Contacts: t.me/gabbo_xl (Telegram)
+*
+*Copyright (C) 2018  Gabboxl
+*
+*	 This program (LinkShorter) is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Affero General Public License as published
+*    by the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program (LinkShorter) is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
 
 
 
