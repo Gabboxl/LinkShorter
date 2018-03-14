600cda1bd0580718fafb90a5b1db6598224f7151
Gabboxl <gabboxl@virgilio.it>
2018-03-14 19:59:28 +0100

    Added shinkin service
    coming soon functions to manage the errors

--- class-linkshorter.php
+++ class-linkshorter.php
@@ -29,7 +29,7 @@
 	$apiKeyAdfly = '3f885db2f68d068a6015d4f132bf37b8';
 	// Your user id
 	$uIdAdfly = 16175835;
-    
+
 
 #		bitly credentials
 
@@ -37,27 +37,34 @@
     $userbitly = "bitly username here";
     //your bitly apikey
     $keybitly = "bitly apikey here";
-	
+
 #		google crednetials
     // Get API key from : http://code.google.com/apis/console/
     	$apiKeygoogle = 'your api key here';
-		
+
 
 #		adfocus credentials key
 			$adfocKey = "66ebaa219a97eac02d8690addc1d8a4e";
-		
-	
-    
+
+#		shink.me credientials
+			//your shink.me user id
+				$shinkid = "223430";
+		 //your shink.me auth_token
+		 		$shinktoken = "zRoCZ1";
+
+
+
+
 
 	class linkshorter {
 		private $service;
         private $link;
-        
-        
-        
-        
+
+
+
+
 		function __construct($service, $link, $domain = null, $advert_type = null) {
-            
+
             if ($service == "adfly" || $service == "bitly" || $service == "googl" || $service == "adfocus" and $link != "" or $link != null) {
             $this->$service($link, $domain, $advert_type);
         } else {
@@ -66,28 +73,28 @@
 			exit;
         }
  		}
-        
-        
+
+
 	private function setError($msg) {
         $this->error = $msg;
         $this->hasError = true;
     }
-    
+
     function getError() {
         return $this->error;
     }
-    
+
     function getLink() {
     	return $this->response;
     }
-        
+
       private function adfly($url, $domain = 'adf.ly', $advert_type = 'int') {
       global $apiKeyAdfly;
       global $uIdAdfly;
-      
+
 	  // base api url
 	  $api = 'http://api.adf.ly/api.php?';
-	
+
 	  // api queries
 	  $query = array(
 	    'key' => $apiKeyAdfly,
@@ -96,31 +103,44 @@
 	    'domain' => $domain,
 	    'url' => $url
 	  );
-	
+
 	  // full api url with query string
 	  $api = $api . http_build_query($query);
 	  // get data
 	  if ($data = file_get_contents($api))
 	    $this->response = $data;
 	}
-	
-	
+
+
 	private function adfocus($url) {
 		global $adfocKey;
-		
+
 		$adfoch = file_get_contents("http://adfoc.us/api/?key=$adfocKey&url=$url");
-		
+
 		$this->response   =   $adfoch;
-		
-		
-		
+
+
+
 	}
-	
-	
+
+
+	private function shinkin($url) {
+		global $shinkid;
+		global
+
+		$data = file_get_contents("http://shink.me/stxt/0/id/$shinkid/auth_token/$shinktoken?s=$url");
+
+		$this->response = $data;
+
+
+	}
+
+
+
 
 	private function googl($url) {
     global $apiKeygoogle;
-    
+
 	$postData = array('longUrl' => $url);
 	$jsonData = json_encode($postData);
 
@@ -146,33 +166,33 @@
 
 
 	private function bitly($url) {
-    
+
     global $userbitly;
     global $keybitly;
-    
+
 	    $query = array(
 	        "version" => "2.0.1",
 	        "longUrl" => $url,
 	        "login" => $userbitly, // login variable
 	        "apiKey" => $keybitly // api key variable
 	    );
-	
+
 	    $query = http_build_query($query);
-	
+
 	    $ch = curl_init();
 	    curl_setopt($ch, CURLOPT_URL, "http://api.bit.ly/shorten?".$query);
 	    curl_setopt($ch, CURLOPT_HEADER, 0);
 	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	
+
 	    $response = curl_exec($ch);
 	    curl_close($ch);
-	
+
 	    $response = json_decode($response);
-	
+
 	    if($response->errorCode == 0 && $response->statusCode == "OK") {
-        
+
 	        $this->response  =  $response->results->{$url}->shortUrl;
-            
+
 	    } else {
 	        return null;
 	    }
@@ -180,13 +200,5 @@
 
 
 
-   		
+
 }
-
-
-
-
-
-
-
-
