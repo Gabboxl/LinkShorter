39b080afcf5b998acf9a477919f6b75240460869
Gabriele <gabboxl@virgilio.it>
2018-03-31 19:23:58 +0200

    moved credentials variables in settings.php

--- class-linkshorter.php
+++ class-linkshorter.php
@@ -20,32 +20,7 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
-//		adfly credentials
-
-    //Your adfly api key
-    $apiKeyAdfly = '3f885db2f68d068a6015d4f132bf37b8';
-    // Your user id
-    $uIdAdfly = 16175835;
-
-//		bitly credentials
-
-    //your bitly username
-    $userbitly = 'bitly username here';
-    //your bitly apikey
-    $keybitly = 'bitly apikey here';
-
-//		google crednetials
-    // Get API key from : http://code.google.com/apis/console/
-        $apiKeygoogle = 'your api key here';
-
-//		adfocus credentials key
-            $adfocKey = '66ebaa219a97eac02d8690addc1d8a4e';
-
-//		shink.me credientials
-            //your shink.me user id
-                $shinkid = '223430';
-         //your shink.me auth_token
-                $shinktoken = 'zRoCZ1';
+require("settings.php"); //we include all credentials variables
 
     class linkshorter
     {
