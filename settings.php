01ce0b179ca4a05fe3c20a5eeb62c1f544da3170
Gabboxl <gabboxl@virgilio.it>
2018-04-02 13:38:17 +0200

    moved login variables on settings.php

---
+++ settings.php
@@ -0,0 +1,28 @@
+<?php
+
+//		adfly credentials
+
+    //Your adfly api key
+    $apiKeyAdfly = '3f885db2f68d068a6015d4f132bf37b8';
+    // Your user id
+    $uIdAdfly = 16175835;
+
+//		bitly credentials
+
+    //your bitly username
+    $userbitly = 'bitly username here';
+    //your bitly apikey
+    $keybitly = 'bitly apikey here';
+
+//		google crednetials
+    // Get API key from : http://code.google.com/apis/console/
+        $apiKeygoogle = 'your api key here';
+
+//		adfocus credentials key
+            $adfocKey = '66ebaa219a97eac02d8690addc1d8a4e';
+
+//		shink.me credientials
+            //your shink.me user id
+                $shinkid = '223430';
+         //your shink.me auth_token
+                $shinktoken = 'zRoCZ1';
